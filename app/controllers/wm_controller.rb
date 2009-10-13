3d64d97719009a3d697f7a3a66ce004da4f97863
Smolev Denis <neonix2.0b@gmail.com>
2009-10-14 00:27:06 +0400

    Add sms pay migration, controller, html-view

--- app/controllers/wm_controller.rb
+++ app/controllers/wm_controller.rb
@@ -1,13 +1,100 @@
 require 'xmlrpc/client'
 require 'syslog'
 require 'digest/md5'
+require 'csv'
 class WmController < ApplicationController
   include AuthenticatedSystem
-  before_filter :login_from_cookie, :except => [:ahuetdaitedve, :success, :fail]
-  protect_from_forgery :except => [:ahuetdaitedve, :success, :fail] 
+  before_filter :login_from_cookie, :except => [:ahuetdaitedve, :success, :fail, :smspay, :smsimport]
+  protect_from_forgery :except => [:ahuetdaitedve, :success, :fail, :smspay, :smsimport] 
+  
+  def smsimport
+	country="Россия"
+	file_name='tariffs_service_56443.csv'
+	if File.exist?(file_name)
+		File.open(file_name, 'r'){ |file| 
+			str = file.read
+			Smsbil.delete_all
+			str.split("\n").each do |tmp|
+				country=$1 if tmp=~/\A(.+?);\Z/
+				if tmp=~/\A(\d+);(\d+);(.+?);([\d,]+);(\w+);([\d,]+);(\w+)\Z/
+					#	 $1=1121;$2=107;TELE2;$4=1,43;$5=rur;  $6=3; $7=rur
+					#Номер;ID оператора;Оператор;Доход партнера;Валюта партнера;Цена SMS для абонента;Валюта абонента
+					sms = Smsbil.new()
+					sms.country = country
+					sms.phone = $1
+					sms.op_id = $2
+					sms.op_name = $3
+					sms.income = $4
+					sms.price = $6+$7
+					sms.income.gsub!(/[,]/,'.')
+					sms.price.gsub!(/[,]/,'.')
+					sms.save!
+				end
+			end
+		}
+		File.delete(file_name)
+		@string = "файл удален"
+	end
+	@smsbils = Smsbil.find(:all, :order => "country, op_name")
+	#render :text => @smsbil.to_xml
+  end
+  
+  def smspay
+#  <hash>
+#  <skey>5e26c66707624a9eeb8efa794d10c97a</skey>
+#  <msg>ox985</msg>
+#  <smsid>1252037077</smsid>
+#  <cost>4.61134054658</cost>
+#  <date>2009-10-12 16:59:19</date>
+#  <country-id>45909</country-id>
+#  <operator>operator</operator>
+#  <operator-id>299</operator-id>
+#  <action>smspay</action>
+#  <try>1</try>
+#  <num>1121</num>
+#  <controller>wm</controller>
+#  <sign>7de33232637c3617ab5fe153339f1fa7</sign>
+#  <test>1</test>
+#  <cost-rur>170.18199</cost-rur>
+#  <user-id>71111111111</user-id>
+#  <ran>5</ran>
+#  <msg-trans>ox985</msg-trans>
+#</hash>
+	resp = "smsid:#{params[:smsid]}\n"
+	resp += "status:reply\n\n"
+	
+	if ((Digest::MD5.hexdigest("sms-pay-secret-key")).upcase==params[:skey].upcase) and params[:msg]=~/.*?(\d+)/
+		if(User.exists?($1))
+			user = User.find($1)
+			money = (params[:cost_rur]).to_s.to_f
+			domain = user.login+'.oxnull.net'
+			domain = user.domain if not user.domain.nil? and user.domain.size > 3
+			Syslog.open('oxmaind')
+			Syslog.crit("WEBMONEY SMS add #{money.to_s}р to id=#{params[:msg]}(#{domain}). date=#{params[:date]} sign=#{params[:sign]} smsid=#{params[:smsid]}")
+			Syslog.close
+
+			server = XMLRPC::Client.new2("http://89.208.146.80:1979")
+			server.call("add_balance",user.id,money)
+			server = XMLRPC::Client.new2("http://89.208.146.83:1979")
+			#server.call("register_payment",domain, money.to_s )
+			resp += "#{params[:smsid]}[#{domain}]: Спасибо\n"
+		else
+			Syslog.open('oxmaind')
+			Syslog.crit("WEBMONEY SMS error #{params['cost-rur'].to_s}p to id=#{params[:msg]}. date=#{params[:date]} sign=#{params[:sign]} smsid=#{params[:smsid]}")
+			Syslog.close
+			resp += "#{params[:smsid]}: Не верно указан oxID\n"
+		end
+	else
+		Syslog.open('oxmaind')
+		Syslog.crit("WEBMONEY SMS error HASH #{params['cost-rur'].to_s}p to id=#{params[:msg]}. date=#{params[:date]} sign=#{params[:sign]} smsid=#{params[:smsid]}")
+		Syslog.close
+		resp += "#{params[:smsid]}: Непонятная ошибка с хешем\n"
+	end
+	render :text => resp
+  end
+  
   def ahuetdaitedve
     if User.exists?(params[:oxid])
-
       user = User.find(params[:oxid])
       money = (params[:LMI_PAYMENT_AMOUNT]).to_f# + user.money
       domain = user.login+'.oxnull.net'
